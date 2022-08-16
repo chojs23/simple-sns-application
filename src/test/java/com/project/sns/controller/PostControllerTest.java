ffee903ba8fdabf86be1b8e23874dcca2cfd5058
neo <chojs990222@gmail.com>
2022-08-16 18:57:52 +0900

    포스트 수정 기능 테스트 작성

--- src/test/java/com/project/sns/controller/PostControllerTest.java
+++ src/test/java/com/project/sns/controller/PostControllerTest.java
@@ -2,6 +2,7 @@ package com.project.sns.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.sns.controller.request.PostCreateRequest;
+import com.project.sns.controller.request.PostModifyRequest;
 import com.project.sns.controller.request.PostWriteRequest;
 import com.project.sns.controller.request.UserJoinRequest;
 import com.project.sns.exception.ErrorCode;
@@ -57,4 +58,82 @@ public class PostControllerTest {
                 .andDo(print())
                 .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
     }
+
+    @Test
+    @WithAnonymousUser
+    void 포스트수정시_로그인한상태가_아니라면_에러발생() throws Exception {
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+    }
+
+
+    @Test
+    @WithMockUser
+    void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트수정시_수정하려는글이_없다면_에러발생() throws Exception {
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트수정시_데이터베이스_에러_발생시_에러발생() throws Exception {
+        mockMvc.perform(put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest("title", "body"))))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.DATABASE_ERROR.getStatus().value()));
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 포스트삭제시_로그인한상태가_아니라면_에러발생() throws Exception {
+        mockMvc.perform(delete("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_TOKEN.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트삭제시_본인이_작성한_글이_아니라면_에러발생() throws Exception {
+        mockMvc.perform(delete("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트삭제시_수정하려는글이_없다면_에러발생() throws Exception {
+        mockMvc.perform(delete("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.INVALID_PERMISSION.getStatus().value()));
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트삭제시_데이터베이스_에러_발생시_에러발생() throws Exception {
+        mockMvc.perform(delete("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(ErrorCode.DATABASE_ERROR.getStatus().value()));
+    }
+
 }
