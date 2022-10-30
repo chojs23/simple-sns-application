6191583303dd28c9af4a54c9b0ee00da5fbb3e4c
neo <chojs990222@gmail.com>
2022-10-30 21:12:50 +0900

    알람을 kafka를 이용해 비동기적으로 데이터 처리

--- src/main/java/com/project/sns/service/AlarmService.java
+++ src/main/java/com/project/sns/service/AlarmService.java
@@ -2,7 +2,13 @@ package com.project.sns.service;
 
 import com.project.sns.exception.ErrorCode;
 import com.project.sns.exception.SnsApplicationException;
+import com.project.sns.model.AlarmArgs;
+import com.project.sns.model.AlarmType;
+import com.project.sns.model.entity.AlarmEntity;
+import com.project.sns.model.entity.UserEntity;
+import com.project.sns.repository.AlarmEntityRepository;
 import com.project.sns.repository.EmitterRepository;
+import com.project.sns.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,17 +20,24 @@ import java.io.IOException;
 @Slf4j
 @RequiredArgsConstructor
 public class AlarmService {
-
+    private final AlarmEntityRepository alarmEntityRepository;
     private final static Long DEFAULT_TIMEOUT=60L*1000*60;
     private final static String ALARM_NAME = "alarm";
     private final EmitterRepository emitterRepository;
-    public void send(Integer alarmId,Integer userId){
-        emitterRepository.get(userId).ifPresentOrElse(sseEmitter -> {
+    private final UserEntityRepository userEntityRepository;
+    public void send(AlarmType type, AlarmArgs args,Integer receiverUserId){
+        UserEntity user = userEntityRepository.findById(receiverUserId).orElseThrow(() -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND));
+
+        //alarm save
+        AlarmEntity alarmEntity = alarmEntityRepository.save(AlarmEntity.of(user, type, args));
+
+        emitterRepository.get(receiverUserId).ifPresentOrElse(sseEmitter -> {
+
             try {
-                sseEmitter.send(SseEmitter.event().id(alarmId.toString()).name(ALARM_NAME).data("new alarm"));
+                sseEmitter.send(SseEmitter.event().id(alarmEntity.getId().toString()).name(ALARM_NAME).data("new alarm"));
 
             } catch (IOException e) {
-                emitterRepository.delete(userId);
+                emitterRepository.delete(receiverUserId);
                 throw new SnsApplicationException(ErrorCode.ALARM_CONNECT_ERROR);
             }
         }, () -> log.info("No emitter founded"));
--- src/main/java/com/project/sns/service/PostService.java
+++ src/main/java/com/project/sns/service/PostService.java
@@ -7,6 +7,8 @@ import com.project.sns.model.AlarmType;
 import com.project.sns.model.Comment;
 import com.project.sns.model.Post;
 import com.project.sns.model.entity.*;
+import com.project.sns.model.event.AlarmEvent;
+import com.project.sns.producer.AlarmProducer;
 import com.project.sns.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,6 +26,7 @@ public class PostService {
     private final CommentEntityRepository commentEntityRepository;
     private final AlarmEntityRepository alarmEntityRepository;
     private final AlarmService alarmService;
+    private final AlarmProducer alarmProducer;
 
     @Transactional
     public void create(String userName, String title, String body) {
@@ -85,8 +88,13 @@ public class PostService {
         });
 
         likeEntityRepository.save(LikeEntity.of(userEntity, postEntity));
-        AlarmEntity alarmEntity = alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_LIKE_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
-        alarmService.send(alarmEntity.getId(), postEntity.getUser().getId());
+
+        // 사용자가 like 하면 alarm 같은 경우는 사용자의 주된 관심사가 아니기 때문에 알람은 비동기적으로 처리해서 성능 향상 kafka 사용
+        // 알람을 저장하고 보내는 이벤트를 kafka 로 보냄
+//        AlarmEntity alarmEntity = alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_LIKE_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
+//        alarmService.send(alarmEntity.getId(), postEntity.getUser().getId());
+        alarmProducer.send(new AlarmEvent(postEntity.getUser().getId(), AlarmType.NEW_LIKE_ON_POST,
+                new AlarmArgs(userEntity.getId(), postEntity.getId())));
 
     }
 
@@ -105,8 +113,10 @@ public class PostService {
         //comment save
         commentEntityRepository.save(CommentEntity.of(userEntity, postEntity, comment));
 
-        AlarmEntity alarmEntity = alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_COMMENT_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
-        alarmService.send(alarmEntity.getId(), postEntity.getUser().getId());
+        //AlarmEntity alarmEntity = alarmEntityRepository.save(AlarmEntity.of(postEntity.getUser(), AlarmType.NEW_COMMENT_ON_POST, new AlarmArgs(userEntity.getId(), postEntity.getId())));
+        //alarmService.send(alarmEntity.getId(), postEntity.getUser().getId());
+        alarmProducer.send(new AlarmEvent(postEntity.getUser().getId(), AlarmType.NEW_COMMENT_ON_POST,
+                new AlarmArgs(userEntity.getId(), postEntity.getId())));
     }
 
 
